99384791caead64fe9be418aa315bff3eaa574c5
Manish KC <kcmanish89@gmail.com>
2019-08-27 12:56:19 -0700

    finished lab

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,55 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testRootRoute_containsRootPage() throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.get("/"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(
+						org.hamcrest.Matchers.containsString("<h1>Welcome to CodeFellowship!</h1>")));
+	}
+
+	@Test
+	public void testRootRoute_containsLoginPage() throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.get("/login"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(
+						org.hamcrest.Matchers.containsString("Please sign in")));
+	}
+
+	@Test
+	public void testRootRoute_containsSignUpPage() throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.get("/signup"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(
+						org.hamcrest.Matchers.containsString("Register as a new user")));
 	}
 
 }
